1ee34f9097865e4de64daac759f08ea7fe6d475b
AgnieszkaFierek <agnieszka.fierek@bravelab.io>
2020-05-14 00:21:23 +0200

    feat(carousel): add cats

--- src/app/shared/rate-box/rate-box/rate-box.component.ts
+++ src/app/shared/rate-box/rate-box/rate-box.component.ts
@@ -1,15 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-rate-box',
   templateUrl: './rate-box.component.html',
   styleUrls: ['./rate-box.component.scss']
 })
-export class RateBoxComponent {
+export class RateBoxComponent implements OnChanges {
   @Input() rateCurrency: number;
   @Input() rateCurrencyBadge: string;
   @Input() baseCurrencyBadge: string;
+  @Input() imageIndex?: number;
+
+  photoUrl: string;
 
   constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.imageIndex) {
+      const height = `2${this.imageIndex}0`;
+      this.photoUrl = `https://placekitten.com/300/${height}`;
+    }
   }
 }
