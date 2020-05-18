5596bdd54c4e6965ce60f24a41394e89be5be88e
AgnieszkaFierek <agnieszka.fierek@bravelab.io>
2020-05-18 12:39:47 +0200

    fix(carousel): add very simple test

--- src/app/components/exchange-rates/components/exchange-rates-carousel/exchange-rates-carousel.component.spec.ts
+++ src/app/components/exchange-rates/components/exchange-rates-carousel/exchange-rates-carousel.component.spec.ts
@@ -1,25 +1,64 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ExchangeRatesCarouselComponent } from './exchange-rates-carousel.component';
+import {ExchangeRatesCarouselComponent} from './exchange-rates-carousel.component';
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import * as fromExchangeRates from '../../store/reducers/exchange-rates.reducer';
+import {ExchangeRatesService} from '../../services/exchange-rates.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ExchangeRatesCarouselComponent', () => {
   let component: ExchangeRatesCarouselComponent;
   let fixture: ComponentFixture<ExchangeRatesCarouselComponent>;
+  // let store: MockStore<fromExchangeRates.State>;
+  let service: ExchangeRatesService;
+  const ratesMock = {
+    base: 'EUR',
+    date: '2020-05-17',
+    rates: null
+  };
+  const initialState = {rates: ratesMock};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExchangeRatesCarouselComponent ]
+      declarations: [ExchangeRatesCarouselComponent],
+      imports: [HttpClientTestingModule, BrowserAnimationsModule],
+      providers: [provideMockStore({initialState}), ExchangeRatesService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExchangeRatesCarouselComponent);
+    service = TestBed.get(ExchangeRatesService);
     component = fixture.componentInstance;
+    component.ratesGroup = service.exchangeRateGroups;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should rates', () => {
+  //   component.rate$.subscribe(rate => {
+  //     expect(rate.base).toEqual(ratesMock.base);
+  //   });
+  // });
+
+  it('should rates groups', () => {
+    expect(component.ratesGroup[0].baseCurrency).toEqual('EUR');
+  });
+
+  it('rate next index carousel', () => {
+    const basicIndex = 0;
+    component.onNext(basicIndex);
+    expect(component.rateIndex).toEqual(1);
+  });
+
+  // it('rate prev index carousel', () => {
+  //   const basicIndex = 1;
+  //   component.onPreview(basicIndex);
+  //   expect(component.rateIndex).toEqual(0);
+  // });
 });
